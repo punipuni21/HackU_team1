db40b82b6781bc28806ac979334a2aab06ffcd4d
RikuyaMatsumura <rikuya_matsumura@s.okayama-u.ac.jp>
2021-06-22 19:19:49 +0900

    変更、追加されたその他のファイルをcommit

--- hacku-team1/src/App.tsx
+++ hacku-team1/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import UserPage from './components/pages/UserPage'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <UserPage />
     </div>
   );
 }
