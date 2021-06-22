8501fbdb0a750b342ea545607b4bac26e561bee6
punipuni21 <punihobby@gmail.com>
2021-06-22 12:50:37 +0900

    アプリの紹介文，ログインボタンを追加

--- hacku-team1/src/components/molecules/Introduction.tsx
+++ hacku-team1/src/components/molecules/Introduction.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import TextLabel from "../atoms/TextLabel";
+import Button from "../atoms/Button";
+import { Container, Stack } from "@material-ui/core";
 
 const Introduction: React.FC = () => {
+  const handleClickButton = () => {
+    alert("押した");
+  };
   return (
     <>
-      <h2>トップページ</h2>
+      <Container fixed>
+        <TextLabel
+          classname="description"
+          text="ここはアプリの紹介"
+        ></TextLabel>
+        <Stack direction="row" spacing={2}>
+          <Button
+            name="Sign Up"
+            classname="sign-up"
+            onClick={handleClickButton}
+          ></Button>
+          <Button
+            name="Sign In"
+            classname="sign-in"
+            onClick={handleClickButton}
+          ></Button>
+        </Stack>
+      </Container>
+      <TextLabel
+          classname="description"
+          text="みんなに消化されたコンテンツ"
+        ></TextLabel>
     </>
   );
 };
 
-export default TopPage;
+export default Introduction;
--- hacku-team1/src/components/pages/TopPage.tsx
+++ hacku-team1/src/components/pages/TopPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Introduction from "../molecules/Introduction";
 
 const TopPage: React.FC = () => {
   return (
     <>
       <h2>トップページ</h2>
+      <Introduction></Introduction>
     </>
   );
 };
