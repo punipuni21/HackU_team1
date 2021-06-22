018754e49c5b52ce3a1684c4496ec2132318923d
punipuni21 <punihobby@gmail.com>
2021-06-22 13:37:00 +0900

    トップページに消化されたコンテンツ（仮）を表示

--- hacku-team1/src/components/pages/TopPage.tsx
+++ hacku-team1/src/components/pages/TopPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Introduction from "../molecules/Introduction";
+import ContentList from "../organisms/ContentList";
+import { Container } from "@material-ui/core";
 
 const TopPage: React.FC = () => {
   return (
     <>
-      <h2>トップページ</h2>
-      <Introduction></Introduction>
+      <Container>
+        <h2>トップページ</h2>
+        <Introduction></Introduction>
+        <ContentList></ContentList>
+      </Container>
     </>
   );
 };
