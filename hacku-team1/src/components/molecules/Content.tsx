7357668b7acc6d1fae755c56fa3989d79b2f7463
punipuni21 <punihobby@gmail.com>
2021-06-22 21:46:14 +0900

    Stackを削除

--- hacku-team1/src/components/molecules/Content.tsx
+++ hacku-team1/src/components/molecules/Content.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "../atoms/Image";
 import TextLabel from "../atoms/TextLabel";
-import { Stack } from "@material-ui/core";
 import { classicNameResolver } from "typescript";
 
 type Props = {
@@ -14,14 +13,14 @@ type Props = {
 const Content: React.FC<Props> = ({ src, alt, classname, text }) => {
   return (
     <>
-      <Stack>
-        <Image
+      <div>
+      <Image
           src={src}
           alt={alt}
           classname={classname}
         ></Image>
         <TextLabel classname={classname} text={text}></TextLabel>
-      </Stack>
+      </div>
     </>
   );
 };
