21e3686f6e48edf8b1dde63bc6f2f77bcccf134d
punipuni21 <punihobby@gmail.com>
2021-06-22 21:48:40 +0900

    StackをGridに変更

--- hacku-team1/src/components/molecules/Introduction.tsx
+++ hacku-team1/src/components/molecules/Introduction.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import TextLabel from "../atoms/TextLabel";
 import Button from "../atoms/Button";
-import { Container, Stack } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const Introduction: React.FC = () => {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
   const handleClickButton = () => {
     alert("押した");
   };
@@ -14,18 +31,22 @@ const Introduction: React.FC = () => {
           classname="description"
           text="ここはアプリの紹介"
         ></TextLabel>
-        <Stack direction="row" spacing={2}>
-          <Button
-            name="Sign Up"
-            classname="sign-up"
-            onClick={handleClickButton}
-          ></Button>
-          <Button
-            name="Sign In"
-            classname="sign-in"
-            onClick={handleClickButton}
-          ></Button>
-        </Stack>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="center">
+              <Button
+                name="Sign Up"
+                classname="sign-up"
+                onClick={handleClickButton}
+              ></Button>
+              <Button
+                name="Sign In"
+                classname="sign-in"
+                onClick={handleClickButton}
+              ></Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
       <TextLabel
         classname="description"
