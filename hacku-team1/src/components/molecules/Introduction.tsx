33fe7313e168fc5bacbdab52a3ad7d0fa8e507d8
punipuni21 <punihobby@gmail.com>
2021-06-23 21:34:55 +0900

    cssを編集

--- hacku-team1/src/components/molecules/Introduction.tsx
+++ hacku-team1/src/components/molecules/Introduction.tsx
@@ -1,21 +1,104 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextLabel from "../atoms/TextLabel";
 import Button from "../atoms/Button";
 import { Container, Grid } from "@material-ui/core";
+import { normalize } from "path/posix";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing(2),
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    introduction: {
+      width: "100%",
+      // height: "404px ",
+      position: "relative",
+      left: "0%",
+      right: "0%",
+      top: "12.68%",
+      bottom: "55.18%",
+      background: "#BBDEFB",
+      opacity: "0.5",
+      paddingBottom: "5%",
+    },
+    textLabel: {
+      position: "relative",
+      width: "730px",
+      height: "56px",
+      left: "calc(50% - 730px/2)",
+      top: "0px",
+      /* H2 - bold 44 (56, 0.2px) */
+      fontFamily: "Mulish",
+      fontStyle: "normal",
+      fontWeight: "bold",
+      fontSize: "44px",
+      lineHeight: "56px",
+      /* identical to box height, or 127% */
+      textAlign: "center",
+      letterSpacing: "0.2px",
+      /* Light / Black */
+      color: "black",
+      /* Inside Auto Layout */
+      flex: "none",
+      order: 0,
+      flexGrow: 0,
+      margin: "0",
+      padding: "3% 0",
+    },
+    SignUp: {
+      /* Auto Layout */
+      display: "inherit",
+      flexDirection: "row",
+      alignItems: "center",
+      padding: "12px 18px",
+
+      position: "static",
+      width: "91px",
+      height: "42px",
+      left: "calc(50% - 91px/2 - 54px)",
+      top: "0px",
+
+      /* Accent / Default */
+      background: "#3C64B1",
+
+      /* Inside Auto Layout */
+      flex: "none",
+      order: 0,
+      flexGrow: 0,
+      margin: "0px 24px",
+    },
+    SignIn: {
+      position: "inherit",
+      width: "91px",
+      height: "42px",
+      left: "calc(50% - 91px/2 - 54px)",
+      right: "18px",
+      top: "calc(50% - 18px/2)",
+
+      /* Button - bold 14 (18, 0.3px) */
+
+      fontFamily: "Mulish",
+      fontStyle: "normal",
+      fontWeight: "bold",
+      fontSize: "14px",
+      lineHeight: "18px",
+
+      /* identical to box height, or 129% */
+      // display: "flex",
+      alignItems: "center",
+      textAlign: "center",
+      letterSpacing: "0.3px",
+
+      /* Accent / Default */
+      color: "#3C64B1",
+      background: "#BBDEFB",
+
+      /* Inside Auto Layout */
+      flex: "none",
+      order: 0,
+      flexGrow: 0,
+      margin: "0px 24px",
+    },
+  })
+);
 
 const Introduction: React.FC = () => {
   const [spacing, setSpacing] = React.useState(2);
@@ -26,28 +109,30 @@ const Introduction: React.FC = () => {
   };
   return (
     <div>
-      <Container fixed>
+      <div className={classes.introduction}>
+        {/* <Container> */}
         <TextLabel
-          classname="description"
+          classname={classes.textLabel}
           text="ここはアプリの紹介"
         ></TextLabel>
-        <Grid container className={classes.root} spacing={2}>
+        <Grid container>
           <Grid item xs={12}>
-            <Grid container justify="center">
+            <Grid container justify="center" spacing={3}>
               <Button
                 name="Sign Up"
-                classname="sign-up"
+                classname={classes.SignUp}
                 onClick={handleClickButton}
               ></Button>
               <Button
                 name="Sign In"
-                classname="sign-in"
+                classname={classes.SignIn}
                 onClick={handleClickButton}
               ></Button>
             </Grid>
           </Grid>
         </Grid>
-      </Container>
+        {/* </Container> */}
+      </div>
       <TextLabel
         classname="description"
         text="みんなに消化されたコンテンツ"
