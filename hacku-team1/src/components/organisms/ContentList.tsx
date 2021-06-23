5e730cfa3cf41100cd1eb4ed1d31b15431f5c17b
punipuni21 <punihobby@gmail.com>
2021-06-23 21:49:02 +0900

    不必要な装飾を削除

--- hacku-team1/src/components/organisms/ContentList.tsx
+++ hacku-team1/src/components/organisms/ContentList.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import Image from "../atoms/Image";
-import TextLabel from "../atoms/TextLabel";
 import Content from "../molecules/Content";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing(2),
   },
 }));
 
