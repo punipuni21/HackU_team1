7684b6814af1c6e0a1767cebaa82afe45fbf4668
punipuni21 <punihobby@gmail.com>
2021-06-26 22:48:29 +0900

    いらないimportの削除

--- hacku-team1/src/components/molecules/Introduction.tsx
+++ hacku-team1/src/components/molecules/Introduction.tsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextLabel from "../atoms/TextLabel";
-import Button from "../atoms/Button";
-import { Grid } from "@material-ui/core";
-import { GoogleLoginButton } from "react-social-login-buttons";
-
-import firebase from "../../firebase/firebase";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
