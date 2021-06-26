def02f461be270e0da62475e95d5bf421db6d35d
punipuni21 <punihobby@gmail.com>
2021-06-26 22:43:42 +0900

    cssを編集

--- hacku-team1/src/components/molecules/Introduction.tsx
+++ hacku-team1/src/components/molecules/Introduction.tsx
@@ -12,6 +12,7 @@ const useStyles = makeStyles((theme: Theme) =>
     introduction: {
       width: "100%",
       // height: "404px ",
+      textAlign: "center",
       position: "relative",
       left: "0%",
       right: "0%",
@@ -21,9 +22,9 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingBottom: "5%",
     },
     textLabel: {
-      position: "relative",
-      width: "730px",
-      height: "56px",
+      // position: "relative",
+      // width: "730px",
+      // height: "56px",
       left: "calc(50% - 730px/2)",
       top: "0px",
       /* H2 - bold 44 (56, 0.2px) */
@@ -44,64 +45,6 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: "0",
       padding: "3% 0",
     },
-    SignUp: {
-      /* Auto Layout */
-      display: "inherit",
-      flexDirection: "row",
-      alignItems: "center",
-      padding: "12px 18px",
-
-      position: "static",
-      width: "91px",
-      height: "42px",
-      left: "calc(50% - 91px/2 - 54px)",
-      top: "0px",
-
-      /* Accent / Default */
-      background: "#3C64B1",
-      color: "white",
-      fontWeight: "bold",
-      borderColor: "#3C64B1",
-
-      /* Inside Auto Layout */
-      flex: "none",
-      order: 0,
-      flexGrow: 0,
-      margin: "0px 24px",
-    },
-    SignIn: {
-      position: "inherit",
-      width: "91px",
-      height: "42px",
-      left: "calc(50% - 91px/2 - 54px)",
-      right: "18px",
-      top: "calc(50% - 18px/2)",
-
-      /* Button - bold 14 (18, 0.3px) */
-
-      fontFamily: "Mulish",
-      fontStyle: "normal",
-      fontWeight: "bold",
-      fontSize: "14px",
-      lineHeight: "18px",
-
-      /* identical to box height, or 129% */
-      // display: "flex",
-      alignItems: "center",
-      textAlign: "center",
-      letterSpacing: "0.3px",
-
-      /* Accent / Default */
-      color: "#3C64B1",
-      borderColor: "#3C64B1",
-      background: "#BBDEFB",
-
-      /* Inside Auto Layout */
-      flex: "none",
-      order: 0,
-      flexGrow: 0,
-      margin: "0px 24px",
-    },
   })
 );
 
@@ -110,18 +53,11 @@ const Introduction: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <div>
-      <div className={classes.introduction}>
-        <TextLabel
-          classname={classes.textLabel}
-          text="ここはアプリの紹介"
-        ></TextLabel>
-        <Grid container>
-          <Grid item xs={12}>
-            <Grid container justify="center" spacing={3}></Grid>
-          </Grid>
-        </Grid>
-      </div>
+    <div className={classes.introduction}>
+      <TextLabel
+        classname={classes.textLabel}
+        text="ここはアプリの紹介"
+      ></TextLabel>
     </div>
   );
 };
