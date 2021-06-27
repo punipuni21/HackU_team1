926389ea99f56d14cf90517685ad625c26f5f7fb
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2021-06-28 01:57:16 +0900

    OtherUserspageのレイアウトを調整

--- hacku-team1/src/components/molecules/SearchBar.tsx
+++ hacku-team1/src/components/molecules/SearchBar.tsx
@@ -2,50 +2,64 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import SearchIcon from "@material-ui/icons/Search";
 import Button from "@material-ui/core/Button";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-        "& > *": {
-          width: "25ch",
-        },
+      "& > *": {
+        width: "25ch",
+      },
     },
     searchBox: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+      display: "flex",
+      // alignItems: "center",
+      justifyContent: "center",
     },
     searchIcon: {
-        fontSize: "30px",
+      fontSize: "xx-large",
+    },
+    searchButton: {
+      borderRadius: "0px 4px 4px 0px",
+      boxShadow: "none",
+    },
+    textfield: {
+      [`& fieldset`]: {
+        borderRadius: "4px 0px 0px 4px",
+      },
     },
   })
 );
 
-type Props= {
-  text: string; 
+type Props = {
+  text: string;
   onChange: any;
   onClick: any;
-}
+};
 
-const SearchBar: React.FC<Props> = ({text, onChange, onClick}) => {
+const SearchBar: React.FC<Props> = ({ text, onChange, onClick }) => {
   const classes = useStyles();
   return (
     <div className={classes.searchBox}>
       <form noValidate className={classes.root} autoComplete="off">
-        <TextField 
+        <TextField
           id="outlined-basic"
           value={text}
-          label="ユーザー名" 
-          fullWidth 
-          variant="outlined" 
+          label="ユーザー名"
+          fullWidth
+          variant="outlined"
           onChange={onChange}
+          className={classes.textfield}
         />
       </form>
 
-      <Button onClick={onClick} variant="contained">
-     <SearchIcon className={classes.searchIcon} />
-       </Button>
+      <Button
+        onClick={onClick}
+        variant="contained"
+        className={classes.searchButton}
+      >
+        <SearchIcon className={classes.searchIcon} />
+      </Button>
     </div>
   );
 };
--- hacku-team1/src/components/molecules/UserBlock.tsx
+++ hacku-team1/src/components/molecules/UserBlock.tsx
@@ -5,56 +5,63 @@ import Chip from "@material-ui/core/Chip";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     avatar: {
-      width: "100px",
-      height: "100px",
-      margin: "1em auto"
+      width: "fit-content",
+      height: "fit-content",
+      margin: "1em auto",
     },
-    typography: {},
-    tags: {
-      margin: "0 auto",
+    typography: {
+      margin: "1rem 0",
     },
-    tag: {
-      margin: "1em",
+
+    tag1: { marginRight: "0.25rem" },
+    tag2: { marginLeft: "0.25rem" },
+
+    overrall: {
+      width: "40vh",
     },
-  }),
+    chips: {},
+  })
 );
 
 type Props = {
   name: string;
   icon: string;
-  tag1: string; 
+  tag1: string;
   tag2: string;
 };
 
 const UserBlock: React.FC<Props> = ({ name, icon, tag1, tag2 }) => {
   const classes = useStyles();
   return (
-    <>
-      <Button onClick={() => alert("ユーザを押した")}>
-        <Container maxWidth="sm">
-          <Avatar alt={name} src={icon} className={classes.avatar} />
-          <Typography variant="h5" className={classes.typography}>
-            {name}
-          </Typography>
-          <Chip
-            variant="outlined"
-            size="small"
-            label={tag1}
-            className={classes.tag}
-          />
-          <Chip
-            variant="outlined"
-            size="small"
-            label={tag2}
-            className={classes.tag}
-          />
-        </Container>
-      </Button>
-    </>
+    <Button
+      onClick={() => alert("ユーザを押した")}
+      className={classes.overrall}
+    >
+      <Container maxWidth="sm">
+        <Avatar alt={name} src={icon} className={classes.avatar} />
+        <Typography variant="h5" className={classes.typography}>
+          {name}
+        </Typography>
+        <Chip
+          variant="outlined"
+          size="small"
+          label={tag1}
+          className={classes.tag1}
+        />
+        <Chip
+          variant="outlined"
+          size="small"
+          label={tag2}
+          className={classes.tag2}
+        />
+      </Container>
+    </Button>
   );
 };
 
