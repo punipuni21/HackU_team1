0e33749e50a9471b83fbbcdb37dbf82ad629fb08
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2021-06-30 12:11:10 +0900

    スマホFooterの色付けとアプリ名変更

--- hacku-team1/src/components/molecules/AppName.tsx
+++ hacku-team1/src/components/molecules/AppName.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 type Props = {
   className: string;
-}
+};
 
 const AppName: React.FC<Props> = ({ className }) => {
   return (
     <Typography className={className} variant="h4" noWrap>
-      AppName
+      しろ-せん
     </Typography>
-  )
-}
+  );
+};
 
 export default AppName;
